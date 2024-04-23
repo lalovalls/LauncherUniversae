492ba448279235af5d64b28863b50b0ef2330cd2
lalovalls <163852077+lalovalls@users.noreply.github.com>
2024-04-23 10:09:06 +0200

    CuartoPush
    agrego los 14 iconos de los diferentes grados

--- LauncherUniversae/src/main/LauncherBase.java
+++ LauncherUniversae/src/main/LauncherBase.java
@@ -23,7 +23,21 @@ public class LauncherBase extends javax.swing.JFrame {
         initComponents();
         this.setLocationRelativeTo(this);
         
-        SetImageLabel(JLabel1, "src/image1/FondoBase.png");
+        SetImageLabel(Fondo, "src/image1/FondoBase.png");
+        SetImageLabel(Escudo8, "src/image1/LauncherButton8.png");
+        SetImageLabel(Escudo0, "src/image1/LauncherButton0.png");
+        SetImageLabel(Escudo1, "src/image1/LauncherButton1.png");
+        SetImageLabel(Escudo2, "src/image1/LauncherButton2.png");
+        SetImageLabel(Escudo3, "src/image1/LauncherButton3.png");
+        SetImageLabel(Escudo4, "src/image1/LauncherButton4.png");
+        SetImageLabel(Escudo5, "src/image1/LauncherButton5.png");
+        SetImageLabel(Escudo6, "src/image1/LauncherButton6.png");
+        SetImageLabel(Escudo7, "src/image1/LauncherButton7.png");
+        SetImageLabel(Escudo13, "src/image1/LauncherButton13.png");
+        SetImageLabel(Escudo9, "src/image1/LauncherButton9.png");
+        SetImageLabel(Escudo10, "src/image1/LauncherButton10.png");
+        SetImageLabel(Escudo11, "src/image1/LauncherButton11.png");
+        SetImageLabel(Escudo12, "src/image1/LauncherButton12.png");
     }
 
     
@@ -32,21 +46,71 @@ public class LauncherBase extends javax.swing.JFrame {
     private void initComponents() {
 
         bg = new javax.swing.JPanel();
-        JLabel1 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
+        Escudo8 = new javax.swing.JLabel();
+        Escudo0 = new javax.swing.JLabel();
+        Escudo1 = new javax.swing.JLabel();
+        Escudo2 = new javax.swing.JLabel();
+        Escudo3 = new javax.swing.JLabel();
+        Escudo4 = new javax.swing.JLabel();
+        Escudo5 = new javax.swing.JLabel();
+        Escudo6 = new javax.swing.JLabel();
+        Escudo7 = new javax.swing.JLabel();
+        Escudo13 = new javax.swing.JLabel();
+        Escudo9 = new javax.swing.JLabel();
+        Escudo10 = new javax.swing.JLabel();
+        Escudo11 = new javax.swing.JLabel();
+        Escudo12 = new javax.swing.JLabel();
+        Fondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setLocationByPlatform(true);
-        setUndecorated(true);
-        setResizable(false);
 
         bg.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        JLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image1/FondoBase.png"))); // NOI18N
-        bg.add(JLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-2, -4, 1920, 1080));
+        Escudo8.setBorder(new javax.swing.border.MatteBorder(null));
+        bg.add(Escudo8, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 80, 103, 111));
 
-        jLabel1.setText("jLabel1");
-        bg.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 70, 1540, 110));
+        Escudo0.setBorder(new javax.swing.border.MatteBorder(null));
+        bg.add(Escudo0, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 80, 103, 111));
+
+        Escudo1.setBorder(new javax.swing.border.MatteBorder(null));
+        bg.add(Escudo1, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 80, 103, 111));
+
+        Escudo2.setBorder(new javax.swing.border.MatteBorder(null));
+        bg.add(Escudo2, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 80, 103, 111));
+
+        Escudo3.setBorder(new javax.swing.border.MatteBorder(null));
+        bg.add(Escudo3, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 80, 103, 111));
+
+        Escudo4.setBorder(new javax.swing.border.MatteBorder(null));
+        bg.add(Escudo4, new org.netbeans.lib.awtextra.AbsoluteConstraints(930, 80, 103, 111));
+
+        Escudo5.setBorder(new javax.swing.border.MatteBorder(null));
+        bg.add(Escudo5, new org.netbeans.lib.awtextra.AbsoluteConstraints(1040, 80, 103, 111));
+
+        Escudo6.setBorder(new javax.swing.border.MatteBorder(null));
+        bg.add(Escudo6, new org.netbeans.lib.awtextra.AbsoluteConstraints(1150, 80, 103, 111));
+
+        Escudo7.setBorder(new javax.swing.border.MatteBorder(null));
+        bg.add(Escudo7, new org.netbeans.lib.awtextra.AbsoluteConstraints(1260, 80, 103, 111));
+
+        Escudo13.setBorder(new javax.swing.border.MatteBorder(null));
+        bg.add(Escudo13, new org.netbeans.lib.awtextra.AbsoluteConstraints(1370, 80, 103, 111));
+
+        Escudo9.setBorder(new javax.swing.border.MatteBorder(null));
+        bg.add(Escudo9, new org.netbeans.lib.awtextra.AbsoluteConstraints(1480, 80, 103, 111));
+
+        Escudo10.setBorder(new javax.swing.border.MatteBorder(null));
+        bg.add(Escudo10, new org.netbeans.lib.awtextra.AbsoluteConstraints(1590, 80, 103, 111));
+
+        Escudo11.setBorder(new javax.swing.border.MatteBorder(null));
+        bg.add(Escudo11, new org.netbeans.lib.awtextra.AbsoluteConstraints(1700, 80, 103, 111));
+
+        Escudo12.setBorder(new javax.swing.border.MatteBorder(null));
+        bg.add(Escudo12, new org.netbeans.lib.awtextra.AbsoluteConstraints(1810, 80, 103, 111));
+
+        Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image1/FondoBase.png"))); // NOI18N
+        bg.add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(-2, -4, 1920, 1080));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -98,8 +162,21 @@ public class LauncherBase extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel JLabel1;
+    private javax.swing.JLabel Escudo0;
+    private javax.swing.JLabel Escudo1;
+    private javax.swing.JLabel Escudo10;
+    private javax.swing.JLabel Escudo11;
+    private javax.swing.JLabel Escudo12;
+    private javax.swing.JLabel Escudo13;
+    private javax.swing.JLabel Escudo2;
+    private javax.swing.JLabel Escudo3;
+    private javax.swing.JLabel Escudo4;
+    private javax.swing.JLabel Escudo5;
+    private javax.swing.JLabel Escudo6;
+    private javax.swing.JLabel Escudo7;
+    private javax.swing.JLabel Escudo8;
+    private javax.swing.JLabel Escudo9;
+    private javax.swing.JLabel Fondo;
     private javax.swing.JPanel bg;
-    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
