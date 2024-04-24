76b39fbd4bdb62aca2e2ab58f35c35d1308ae771
l4rry9912 <162343742+l4rry9912@users.noreply.github.com>
2024-04-24 11:50:38 +0200

    tercerpush

--- LauncherUniversae/src/main/HomeController.java
+++ LauncherUniversae/src/main/HomeController.java
@@ -4,11 +4,10 @@
  */
 package main;
 
-import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
@@ -21,7 +20,17 @@ public class HomeController extends javax.swing.JPanel {
     
     public HomeController() {
         initComponents();
-        crearGrid();
+          addComponentListener(new ComponentAdapter() {
+        @Override
+        public void componentResized(ComponentEvent e) {
+            setImageLabel(jLabel1);
+            setImageLabel(jLabel2);
+            setImageLabel(jLabel3);
+            setImageLabel(jLabel4);
+            setImageLabel(jLabel5);
+            setImageLabel(jLabel6);
+        }
+    });
     }
     
 
@@ -30,21 +39,41 @@ public class HomeController extends javax.swing.JPanel {
     private void initComponents() {
 
         bg = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
 
         setMinimumSize(new java.awt.Dimension(1540, 870));
 
         bg.setBackground(new java.awt.Color(10, 38, 72));
+        bg.setLayout(new java.awt.GridLayout(2, 3, 60, 60));
 
-        javax.swing.GroupLayout bgLayout = new javax.swing.GroupLayout(bg);
-        bg.setLayout(bgLayout);
-        bgLayout.setHorizontalGroup(
-            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1540, Short.MAX_VALUE)
-        );
-        bgLayout.setVerticalGroup(
-            bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 870, Short.MAX_VALUE)
-        );
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/HomeButton0.png"))); // NOI18N
+        bg.add(jLabel1);
+
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/HomeButton0.png"))); // NOI18N
+        bg.add(jLabel2);
+
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/HomeButton0.png"))); // NOI18N
+        bg.add(jLabel3);
+
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/HomeButton0.png"))); // NOI18N
+        bg.add(jLabel4);
+
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/HomeButton0.png"))); // NOI18N
+        bg.add(jLabel5);
+
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/HomeButton0.png"))); // NOI18N
+        bg.add(jLabel6);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -57,52 +86,26 @@ public class HomeController extends javax.swing.JPanel {
             .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
-
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel bg;
-    // End of variables declaration//GEN-END:variables
-private void crearGrid() {
-    // Crear GridLayout con 2 filas y 3 columnas
-    GridLayout gridLayout = new GridLayout(2, 3);
-    gridLayout.setHgap(80); 
-    gridLayout.setVgap(80);
-    
-    bg.setLayout(gridLayout);
-
-    // Rutas de las imágenes
-    String[] imagePaths = {
-        "src/imagenes/HomeButton0.png",
-        "src/imagenes/HomeButton1.png",
-        "src/imagenes/HomeButton2.png",
-        "src/imagenes/HomeButton3.png",
-        "src/imagenes/HomeButton4.png",
-        "src/imagenes/HomeButton5.png"
-    };
-
-    // Agregar JLabel al JPanel bg con imágenes
-    for (String imagePath : imagePaths) {
-        JLabel label = new JLabel();
-        label.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                setImageLabel(label, imagePath);
-            }
-        });
-        
-        bg.add(label);
+  private void setImageLabel(JLabel labelName) {
+    Icon icon = labelName.getIcon(); 
+    if (icon != null && icon instanceof ImageIcon) { 
+        ImageIcon imageIcon = (ImageIcon) icon;
+        Image image = imageIcon.getImage();
+        Image scaledImage = image.getScaledInstance(labelName.getWidth(), labelName.getHeight(), Image.SCALE_DEFAULT);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        labelName.setIcon(scaledIcon);
+        this.repaint();
     }
 }
 
-private void setImageLabel(JLabel labelName, String root) {
-    ImageIcon imageIcon = new ImageIcon(root);
-        Image image = imageIcon.getImage().getScaledInstance(labelName.getWidth(), labelName.getHeight(), Image.SCALE_DEFAULT);
-        ImageIcon scaledIcon = new ImageIcon(image);
-        labelName.setIcon(scaledIcon);
-        labelName.repaint();
-    
-}
-
-
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel bg;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    // End of variables declaration//GEN-END:variables
 }
 
